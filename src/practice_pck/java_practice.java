44436817cb20f2b4ab703dbf352b13efc4b66bff
Chaman-preet <chaman.preet@evalueserve.com>
2019-01-19 01:06:32 +0530

    Update java_practice.java
    add java programs on 19th jan 2019

--- src/practice_pck/java_practice.java
+++ src/practice_pck/java_practice.java
@@ -120,3 +120,143 @@ public void countcharacter()
 //    System.out.println(count);
 }
 	}
+
+@Test
+//Reverse String wo string builder
+public void ReverseString
+{
+        System.out.println("Enter string to reverse:");
+        
+        Scanner read = new Scanner(System.in);
+        String str = read.nextLine();
+        String reverse = "";
+        
+        
+        for(int i = str.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + str.charAt(i);
+        }
+        
+        System.out.println("Reversed string is:");
+        System.out.println(reverse);
+    }
+}
+
+@Test
+//Reverse String with stringbuilder
+    public void reversebuilder()
+    { 
+        String input = "Geeks for Geeks"; 
+  
+        StringBuilder input1 = new StringBuilder(); 
+  
+        // append a string into StringBuilder input1 
+        input1.append(input); 
+  
+        // reverse StringBuilder input1 
+        input1 = input1.reverse(); 
+  
+        // print reversed String 
+        System.out.println(input1); 
+    } 
+
+@Test
+//Find duplicate characters in a string
+
+ public void findduplicate() {
+  String str = "java2blog.com ";
+  HashMap charCountMap = new HashMap();
+  for (int i = 0; i < str.length(); i++) {
+   char c = str.charAt(i);
+ 
+   if (charCountMap.containsKey(c)) {
+    charCountMap.put(c, charCountMap.get(c) + 1);
+   } else {
+    charCountMap.put(c, 1);
+   }
+ 
+  }
+  for (Character c : charCountMap.keySet()) {
+   if (charCountMap.get(c) > 1)
+    System.out.println("duplicate character : " + c + " ====== " + " count : " + charCountMap.get(c));
+ 
+  }
+ 
+ }
+
+@Test
+//Find maximum of 2 arrays
+   public void maxarray() {  
+    {   int[] arr = { 2, 3, 5, 2, 6, 7, 2, 5, 8 };  
+        int max = 0;  
+        int secondMax = 0;  
+  
+        for (int i : arr)  
+            if (i > max) {  
+                secondMax = max;  
+                max = i;  
+            } else if (i > secondMax)  
+                secondMax = i;  
+  
+        System.out.println("Max: " + max + " Second Max: " + secondMax);  
+    }  
+ 
+}
+
+@Test
+//count occurence of a character in string
+
+public void int countOccurrences(String str, char ch) {
+    int counter = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == ch) {
+            counter++;
+        }
+    }
+    return counter;
+} 
+
+@Test
+//count occurence of all characters using hashmap
+  static void characterCount(String inputString)
+    {
+	  String str = "geeksforgeeks"; 
+        //Creating a HashMap containing char as a key and occurrences as  a value
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+        //Converting given string to char array
+        char[] strArray = inputString.toCharArray();
+        //checking each char of strArray
+        for (char c : strArray)
+        {
+            if(charCountMap.containsKey(c))
+            {
+                //If char is present in charCountMap, incrementing it's count by 1
+                charCountMap.put(c, charCountMap.get(c)+1);
+            }
+            else
+            {
+                //If char is not present in charCountMap,
+                //putting this char to charCountMap with 1 as it's value
+                charCountMap.put(c, 1);
+            }
+        }
+        //Printing the charCountMap
+        System.out.println(charCountMap);
+    }
+
+@Test
+//find odd and even
+public void oddeven()
+   {
+      int x;
+      System.out.println("Enter an integer to check if it is odd or even");
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+     
+      if (x % 2 == 0)
+         System.out.println("The number is even.");
+      else
+         System.out.println("The number is odd.");
+   }
+}
+}
