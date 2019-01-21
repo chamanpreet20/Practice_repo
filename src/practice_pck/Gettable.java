609be5392369ba1d4370516a359ee6da5dd118ec
Chaman-preet <chaman.preet@evalueserve.com>
2019-01-21 11:00:07 +0530

    Update Gettable.java
    additional changes on 21 jan 2019

--- src/practice_pck/Gettable.java
+++ src/practice_pck/Gettable.java
@@ -33,6 +33,23 @@ for(j=0;j<columns.size();j++)
 	System.out.println("Columns are: "+columns.get(j).getText());
 }
 System.out.println ("Table data is " );
-	}
+	
+//*********************** second approach to get table cells***************************************	
 
+//Loop will execute till the last row of table.
+	for (int row = 0; row < rows.size(); row++) {
+	    //To locate columns(cells) of that specific row.
+	    List < WebElement > Columns_row = rows.get(row).findElements(By.tagName("td"));
+	    //To calculate no of columns (cells). In that specific row.
+	    int columns_count = Columns_row.size();
+	    System.out.println("Number of cells In Row " + row + " are " + columns_count);
+	    //Loop will execute till the last cell of that specific row.
+	    for (int column = 0; column < columns_count; column++) {
+	        // To retrieve text from that specific cell.
+	        String celtext = Columns_row.get(column).getText();
+	        System.out.println("Cell Value of row number " + row + " and column number " + column + " Is " + celtext);
+	    }
+	    System.out.println("-------------------------------------------------- ");
+	}
+	}	
 }
